466947a45ebe19d7b6af35b603d08ff32da3c3d1
= <prabin.chaudhary3107@gmail.com>
2026-01-28 10:16:05 +0545

    day 8 read blog both single and all

--- app.js
+++ app.js
@@ -19,14 +19,69 @@ app.get("/",(req,res)=>{
     })
 })
 
+// GET API -> All Blogs
+app.get("/blogs",async (req,res)=>{
+
+    //fetching /reading all blogs from Blog Model
+    const blogs = await Blog.find() // find le array bhitra data read garxa
+
+    // Check if blogs contains data or not
+    if(blogs.length == 0){
+        res.json({
+            status: 404,
+            message : "empty blogs"
+        })
+    }else{
+        res.json({
+            status: 300,
+            message: "Blogs fetched successfully",
+            data : blogs
+        })
+    }   
+})
+
+// GET API -> /blogs/:id (single blog)
+
+app.get("/blogs/:id", async (req,res) => {
+    const id = req.params.id
+           // const {id} = req.params //ALTERNATIVE
+
+    // const blog = await Blog.find({_id : id})  
+    //     if(blog.length == 0){
+    //         res.status(300).json({
+    //             status: 404,
+    //             message : "No blog found"
+    //         })
+    //     }else{
+    //         res.status(300).json({
+    //             message:"Blog fetched successfully",
+    //             data : blog
+    //            })
+    //     }
+//    res.status(300).json({
+//     message:"Blog fetched successfully",
+//     data : blog
+//    })
+
+         //ALTERNATIVE
+         const blog = await Blog.findById(id)
+         if(blog){
+            res.status(300).json({
+                message : "Blog fetched successfully ",
+                data : blog
+            })
+         }else{
+            res.status(404).json({
+                message:"No Blog found"
+            })
+         }
+})
+
 // CREATE Blog API - 
 app.post("/createBlog", async (req,res)=>{
     const title = req.body.title;
     const subTitle = req.body.subTitle;
     const description = req.body.description;
-
-    //alternatives 
-
 
     //Insert to database logic goes here
     await Blog.create({
@@ -40,8 +95,8 @@ app.post("/createBlog", async (req,res)=>{
     })
 })
 
-//Alternative
-// res.status(2000).json({
+//AlLTERNATIVE
+// res.status(300).json({
 //     message : "Blog created successfully"
 // })
 
